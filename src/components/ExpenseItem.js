5a6578b543488b554e881a4de5f379f90916f5c2
Michael Monson <michaelmonson@gmail.com>
2023-09-19 12:41:28 -0600

    Split  ExpenseItem date details into new component   - Created a new 'ExpenseDate' component to alllow ExpenseItem to remain lean.   - Added additional styling to improve look.

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,3 +1,4 @@
+import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 
 function ExpenseItem(props) {
@@ -9,7 +10,7 @@ function ExpenseItem(props) {
 
     return (
         <div className="expense-item">
-            <div>{props.date.toDateString()}</div>
+            <ExpenseDate date = {props.date} />
             <div className="expense-item__description">
                 <h2>{props.title}</h2>
                 <div className="expense-item__price">${props.amount}</div>
