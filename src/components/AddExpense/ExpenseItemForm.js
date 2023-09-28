91a95bf64ce84b0e31325b9ff9b2eff162fa56df
Michael Monson <michaelmonson@gmail.com>
2023-09-27 23:01:05 -0600

    Adding 'useState' hooks for all 3 form properties

--- src/components/AddExpense/ExpenseItemForm.js
+++ src/components/AddExpense/ExpenseItemForm.js
@@ -1,12 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
 import './ExpenseItemForm.css';
 
 const ExpenseForm = () => {
+   const [updatedTitle, setUpdatedTitle] = useState('');
+   const [updatedAmount, setUpdatedAmount] = useState('');
+   const [updatedDate, setUpdatedDate] = useState('');
 
    const titleChangeHandler = (event) => {
-      console.log(event.target.value);
+      //console.log(event.target.value);
+      setUpdatedTitle(event.target.value);
    };
+
+   const amountChangeHandler = (event) => {
+      setUpdatedAmount(event.target.value);
+   }; 
+
+   const dateChangeHandler = (event) => {
+      setUpdatedDate(event.target.value);
+   }; 
 
    return (
       <form>
@@ -17,11 +29,11 @@ const ExpenseForm = () => {
             </div>
             <div className="new-expense__control">
                <label>Amount</label>
-               <input type="number" min="0.01" step="0.01" />
+               <input type="number" min="0.01" step="0.01" onChange={amountChangeHandler} />
             </div>
             <div className="new-expense__control">
                <label>Date</label>
-               <input type="date" min="2001-01-01" max="2025-12-31" />
+               <input type="date" min="2001-01-01" max="2025-12-31" onChange={dateChangeHandler} />
             </div>
             <div className="new-expense__actions">
                <button type="submit">Add Expense</button>
