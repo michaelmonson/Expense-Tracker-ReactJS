e237249e095a743f06d903175d34f453c1ec8a96
Michael Monson <michaelmonson@gmail.com>
2023-10-02 15:45:38 -0600

    Assignment 2 - Working with events and state

--- src/components/Expenses/ExpensesDisplay.js
+++ src/components/Expenses/ExpensesDisplay.js
@@ -1,33 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ExpenseItem from './ExpenseItem';
 import Card from '../Card';
 import './ExpensesDisplay.css';
+import ExpensesFilter from './ExpensesFilter';
+import './ExpensesFilter.css';
 
 function ExpensesDisplay(props) {
+   const [filteredYear, setFilteredYear] = useState('2023');
+
+   const filterChangeHandler = (selectedYear) => {
+      console.log('Expense.js : ' + selectedYear);
+      setFilteredYear(selectedYear);
+   };
 
    return (
-      <Card className="expenses">
-         <ExpenseItem
-            title = {props.expenseList[0].title}
-            amount = {props.expenseList[0].amount}
-            date = {props.expenseList[0].date}
-         />
-         <ExpenseItem
-            title = {props.expenseList[1].title}
-            amount = {props.expenseList[1].amount}
-            date = {props.expenseList[1].date}
-         />
-         <ExpenseItem
-            title = {props.expenseList[2].title}
-            amount = {props.expenseList[2].amount}
-            date = {props.expenseList[2].date}
-         />
-         <ExpenseItem
-            title = {props.expenseList[3].title}
-            amount = {props.expenseList[3].amount}
-            date = {props.expenseList[3].date}
-         />
-      </Card>
+      <div>
+         <Card className="expenses">
+            {/* Pass in 'filteredYear' as selected: An example of React's 'two-way' binding */}
+            <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
+            <ExpenseItem
+               title = {props.expenseList[0].title}
+               amount = {props.expenseList[0].amount}
+               date = {props.expenseList[0].date}
+            />
+            <ExpenseItem
+               title = {props.expenseList[1].title}
+               amount = {props.expenseList[1].amount}
+               date = {props.expenseList[1].date}
+            />
+            <ExpenseItem
+               title = {props.expenseList[2].title}
+               amount = {props.expenseList[2].amount}
+               date = {props.expenseList[2].date}
+            />
+            <ExpenseItem
+               title = {props.expenseList[3].title}
+               amount = {props.expenseList[3].amount}
+               date = {props.expenseList[3].date}
+            />
+         </Card>
+      </div>
    );
 }
 
